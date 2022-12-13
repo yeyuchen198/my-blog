3b5d35d691b1a1ae716bb6335cf7bb1965abd311
yuchen3000 <120295644+yuchen3000@users.noreply.github.com>
2022-12-13 10:08:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -36,12 +36,13 @@ cat << EOF > /etc/xray/config.json
 EOF
 
 #run xray
+# xray -config=/etc/xray/config.json &
 xray -config=/etc/xray/config.json &
 
 #run nginx
 # CMD ["nginx", "-g", "daemon off;"]
-# nginx -g 'daemon off;'
-/usr/local/nginx/sbin/nginx -g 'daemon off;'
+nginx -g 'daemon off;'
+# /usr/local/nginx/sbin/nginx -g 'daemon off;'
 
 
 
